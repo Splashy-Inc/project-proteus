1f0ec2c38ac5fc04cb88faea350e31f678101a15
avelitj12 <avelitj12@gmail.com>
2025-01-21 08:13:36 -0500

    Generate new sections as player move to the right

--- project-proteus/SCRIPTS/level_tile_layer.gd
+++ project-proteus/SCRIPTS/level_tile_layer.gd
@@ -2,46 +2,57 @@ extends TileMapLayer
 
 @export var generate_random := false
 @export var seed: String
+@export var camera: Camera2D
 var start_point: Vector2
 var player_jump_tiles
+var next_top_left = Vector2i.ZERO
+var window_size: Vector2i
+var tiles_per_window: Vector2i
+var tiles_per_obstacle = 5
+var latest_path_cell: Vector2i
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var base_tile_size = Vector2i(Globals.PLAYER_JUMP_HEIGHT/2,Globals.PLAYER_JUMP_HEIGHT/2)
+	window_size = Vector2i(get_viewport().get_visible_rect().size)
+	tiles_per_window = Vector2i(window_size/base_tile_size)
 	player_jump_tiles = floor(Globals.PLAYER_JUMP_HEIGHT/tile_set.tile_size.y)
+	
 	if seed:
 		seed(seed.hash())
+		
 	if generate_random:
 		clear()
-		var window_size = Vector2i(get_viewport().get_visible_rect().size)
-		var base_tile_set = tile_set.get_source(0)
-		var base_tile_size = Vector2i(Globals.PLAYER_JUMP_HEIGHT/2,Globals.PLAYER_JUMP_HEIGHT/2)
+		var base_tile_set = tile_set.get_source(1)
 		if base_tile_set is TileSetAtlasSource:
 			base_tile_size = base_tile_set.get_tile_texture_region(Vector2i.ZERO).size
-		start_point = generate_section(Vector2i(0,0), Vector2i(window_size/base_tile_size), 5)
+		start_point = generate_section(next_top_left, tiles_per_window, tiles_per_obstacle, latest_path_cell)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if camera and generate_random:
+		var camera_tile = local_to_map(to_local(camera.global_position))
+		if camera_tile.x  > next_top_left.x - tiles_per_window.x * 2:
+			generate_section(next_top_left, next_top_left + tiles_per_window, tiles_per_obstacle, latest_path_cell)
 
-func generate_section(top_left: Vector2i, bottom_right: Vector2i, avg_tiles_per_obstacle: int):
+func generate_section(top_left: Vector2i, bottom_right: Vector2i, avg_tiles_per_obstacle: int, prev_path_cell: Vector2i):
 	var origin = top_left
 	var size = bottom_right - origin
 	var start_position
-	var prev_path_cell
 	
 	for x in size.x:
 		var column_data = Globals.level_column_data_struct.duplicate(true)
 		column_data["height"] = size.y
-		column_data["bottom_cell"] = Vector2i(x,bottom_right.y)
+		column_data["bottom_cell"] = Vector2i(x+origin.x,bottom_right.y)
 		if x > 0 and x % avg_tiles_per_obstacle == 0:
 			column_data["num_obstacles"] += 1
-		if x == origin.x:
+		prev_path_cell = generate_column(column_data, prev_path_cell)
+		if x == 0:
 			column_data["is_start_column"] = true
-			prev_path_cell = generate_column(column_data, null)
 			start_position = to_global(map_to_local(prev_path_cell))
-		else:
-			prev_path_cell = generate_column(column_data, prev_path_cell)
 	set_cells_terrain_connect(get_used_cells_by_id(1),0,0,false)
+	next_top_left = Vector2i(bottom_right.x, next_top_left.y)
+	latest_path_cell = prev_path_cell
 	return start_position
 
 func generate_column(column_data: Dictionary, prev_path_cell) -> Vector2i:
