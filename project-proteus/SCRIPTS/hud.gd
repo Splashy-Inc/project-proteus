fd9d9bd06d3930a59516bf2f257e6104c3609bd8
avelitj12 <avelitj12@gmail.com>
2025-01-26 11:32:08 -0500

    Show main menu via function

--- project-proteus/SCRIPTS/hud.gd
+++ project-proteus/SCRIPTS/hud.gd
@@ -4,7 +4,7 @@ signal start_level
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	show_main_menu()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,3 +12,12 @@ func _process(delta: float) -> void:
 
 func _on_start_level(level_type: Globals.LevelType) -> void:
 	start_level.emit(level_type)
+
+func show_main_menu():
+	hide_menu_buttons()
+	$MenuPanel.show()
+	$MenuPanel/HBoxContainer/MenuSections/MenuButtons/MainMenu.show()
+
+func hide_menu_buttons():
+	for child in $MenuPanel/HBoxContainer/MenuSections/MenuButtons.get_children():
+		child.hide()
