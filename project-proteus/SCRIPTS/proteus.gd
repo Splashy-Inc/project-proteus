0bad783250e6ff3f380bad1a21af94123d77f6aa
avelitj12 <avelitj12@gmail.com>
2025-01-25 21:00:43 -0500

    Suspend while transforming, then accelerate on attack

--- project-proteus/SCRIPTS/proteus.gd
+++ project-proteus/SCRIPTS/proteus.gd
@@ -30,6 +30,8 @@ var launch_queued := false
 var state_machine: AnimationNodeStateMachinePlayback
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+@export var suspend_motion := false
+@export var speed_modifier := 1.0
 
 func _ready():
 	right_attack_center = $AttackCenter.position
@@ -42,7 +44,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		$ProgressBar.hide()
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and not suspend_motion:
 		velocity += get_gravity() / JUMP_TIME * delta
 		
 	match state:
@@ -59,26 +61,28 @@ func _physics_process(delta: float) -> void:
 
 	
 func _move_state(delta: float):
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		# Give an impule that resolves over JUMP_TIME and peak at JUMP_HEIGHT based on current gravity
-		velocity.y = -sqrt(2 * Globals.PLAYER_JUMP_HEIGHT * get_gravity().y / JUMP_TIME)
+	
+	if not suspend_motion:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			# Give an impule that resolves over JUMP_TIME and peak at JUMP_HEIGHT based on current gravity
+			velocity.y = -sqrt(2 * Globals.PLAYER_JUMP_HEIGHT * get_gravity().y / JUMP_TIME)
 
-	# Get the input direction and handle the movement
-	_set_direction(Input.get_axis("left", "right"))
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if not is_on_floor():
-		state_machine.travel("jump")
-	elif velocity.x == 0:
-		state_machine.travel("idle")
-	else:
-		state_machine.travel("run")
-	
-	move_and_slide()
+		# Get the input direction and handle the movement
+		_set_direction(Input.get_axis("left", "right"))
+		if direction:
+			velocity.x = direction * SPEED * speed_modifier
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		if not is_on_floor():
+			state_machine.travel("jump")
+		elif velocity.x == 0:
+			state_machine.travel("idle")
+		else:
+			state_machine.travel("run")
+		
+		move_and_slide()
 	
 func _attack_state():
 	match attack_type:
